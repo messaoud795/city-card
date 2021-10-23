cc32d14f687d47ac4102838a6d5bb2b7bef7f33b
Nabil <messaoudnabil9@gmail.com>
2021-10-23 16:53:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const services = [
+    "studio",
+    "2 rooms appartment",
+    "3 rooms appartment",
+    "4+ rooms appartment",
+    "private room",
+    "shared room",
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <figure className="app__cityCard">
+        <img
+          src="https://d881krhzwma5n.cloudfront.net/assets/hp/our-cities-desktop-milan-286.jpg"
+          alt="city img"
+          className="app__img"
+        />
+        <figcaption className="app__caption">
+          <p>What do you need?</p>
+          <div className="app__services">
+            {services.map((service, i) => (
+              <span key={i} className="app__service">
+                {service}
+              </span>
+            ))}
+          </div>
+        </figcaption>
+      </figure>
     </div>
   );
 }
